68380c7ef7b9990502af73a724fa7923be66bb37
marcusvco <marcuscarvalhodeoliveira@gmail.com>
2024-08-26 13:31:47 -0300

    refac: Alterando para variável global de caminho

--- Codigo/backend/backend/controller/aluno_controller.py
+++ Codigo/backend/backend/controller/aluno_controller.py
@@ -1,14 +1,15 @@
 import json
 from model import Aluno
+from util import CAMINHO_USUARIOS
 
 
 def cadastrar_aluno(nome: str, email: str, senha: str, cpf: str):
     aluno = Aluno(nome=nome, tipo="aluno", email=email, senha=senha, cpf=cpf)
 
-    with open("database/usuarios.json") as f:
+    with open(CAMINHO_USUARIOS) as f:
         data = json.load(f)
 
     data.append(aluno.model_dump())
 
-    with open("database/usuarios.json", "w") as f:
+    with open(CAMINHO_USUARIOS, "w") as f:
         json.dump(data, f, indent=4)
