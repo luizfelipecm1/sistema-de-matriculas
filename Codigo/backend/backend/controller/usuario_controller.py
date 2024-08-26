4a6bb11fc01ccb561a4b7af8b84305da6e96ed95
marcusvco <marcuscarvalhodeoliveira@gmail.com>
2024-08-26 13:33:00 -0300

    feat: CRUD usuários/alunos

--- Codigo/backend/backend/controller/usuario_controller.py
+++ Codigo/backend/backend/controller/usuario_controller.py
@@ -1,13 +1,59 @@
 import json
-from model import Usuario
+from model import Tipo, Usuario
+from util import CAMINHO_USUARIOS, escrever_no_arquivo, ler_arquivo
+
 
 def cadastrar(usuario: Usuario):
-    with open("database/usuarios.json") as f:
-        data = json.load(f)
+    usuarios: dict = ler_arquivo(CAMINHO_USUARIOS)
+    usuario_cadastrado = usuarios.get(usuario.email, None)
 
-    data.append(usuario.model_dump())
+    if usuario_cadastrado != None:
+        print("Email já cadastrado no sistema.")
+        return
 
-    with open("database/usuarios.json", "w") as f:
-        json.dump(data, f, indent=4)
+    usuarios[usuario.email] = usuario.model_dump()
+
+    escrever_no_arquivo(CAMINHO_USUARIOS, usuarios)
 
     print("Usuário cadastrado com sucesso!")
+
+
+def listar(tipo: Tipo):
+    usuarios: dict = ler_arquivo(CAMINHO_USUARIOS)
+
+    for _, usuario in usuarios.items():
+        if usuario["tipo"] == tipo:
+            print(f"Nome: {usuario["nome"]}, Email: {usuario["email"]}, CPF: {usuario["cpf"]}")
+
+    print("Usuários listados com sucesso!")
+
+
+def editar(email_atual: str, usuario: Usuario):
+    usuarios: dict = ler_arquivo(CAMINHO_USUARIOS)
+    
+    if email_atual not in usuarios:
+        print("Este email não está cadastrado.")
+        return
+    
+    usuarios[usuario.email] = usuario.model_dump()
+
+    if usuario.email != email_atual:
+        del usuarios[email_atual]
+
+    escrever_no_arquivo(CAMINHO_USUARIOS, usuarios)
+
+    print("Usuário editado com sucesso!")
+
+
+def remover(email: str):
+    usuarios: dict = ler_arquivo(CAMINHO_USUARIOS)
+
+    if email not in usuarios:
+        print("Este email não está cadastrado.")
+        return
+    
+    del usuarios[email]
+
+    escrever_no_arquivo(CAMINHO_USUARIOS, usuarios)
+
+    print("Usuário removido com sucesso!")
--- Codigo/backend/backend/view/secretario_view.py
+++ Codigo/backend/backend/view/secretario_view.py
@@ -1,7 +1,7 @@
 from controller import usuario_controller
 from functools import partial
-from model import Usuario, Tipo
-from util import divider, menu
+from model import Tipo, Usuario
+from util import divider, menu, pedir_dados_usuario
 from view import login_view
 
 
@@ -21,27 +21,56 @@ def menu_gerencia_alunos():
     menu(
         header="Gerência de Alunos",
         opcoes={
-            "Listar": partial(print, "listar alunos"),
+            "Listar": partial(listar_usuario, tipo=Tipo.Aluno),
             "Cadastrar": partial(cadastrar_usuario, tipo=Tipo.Aluno),
-            "Editar": partial(print, "editar aluno"),
-            "Remover": partial(print, "remover aluno"),
+            "Editar": partial(editar_usuario, tipo=Tipo.Aluno),
+            "Remover": partial(remover_usuario, tipo=Tipo.Aluno),
         },
         voltar=menu_secretario,
     )
+
+
+def listar_usuario(tipo: Tipo):
+    divider()
+    usuario_controller.listar(tipo=tipo)
+
+    chamar_menu_gerencia_usuario(tipo=tipo)
 
 
 def cadastrar_usuario(tipo: Tipo):
     divider()
     print(f"Digite os campos do {tipo.value.capitalize()}")
 
-    nome = input("Nome: ")
-    email = input("Email: ")
-    senha = input("Senha: ")
-    cpf = input("Cpf: ")
+    nome, email, senha, cpf = pedir_dados_usuario()
 
     usuario = Usuario(nome=nome, tipo=tipo, email=email, senha=senha, cpf=cpf)
     usuario_controller.cadastrar(usuario=usuario)
 
+    chamar_menu_gerencia_usuario(tipo=tipo)
+
+
+def editar_usuario(tipo: Tipo):
+    divider()
+    email_atual = input(f"Digite o email atual do {tipo.value} que deseja editar: ")
+
+    print("Agora digite os novos dados")
+    nome, email, senha, cpf = pedir_dados_usuario()
+
+    usuario = Usuario(nome=nome, tipo=tipo, email=email, senha=senha, cpf=cpf)
+    usuario_controller.editar(email_atual=email_atual, usuario=usuario)
+
+    chamar_menu_gerencia_usuario(tipo=tipo)
+
+
+def remover_usuario(tipo: Tipo):
+    divider()
+    email = input(f"Digite o email do {tipo.value} que deseja remover: ")
+    usuario_controller.remover(email=email)
+
+    chamar_menu_gerencia_usuario(tipo=tipo)
+
+
+def chamar_menu_gerencia_usuario(tipo: Tipo):
     match tipo:
         case Tipo.Aluno:
             menu_gerencia_alunos()
