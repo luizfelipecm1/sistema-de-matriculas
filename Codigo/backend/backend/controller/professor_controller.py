5710c09e3e43a0a5abbef68b932439aa1091d18a
luizfelipecm1 <fluiz6501@gmail.com>
2024-08-30 14:48:21 -0300

    #12 [Code] Lista de alunos por disciplina adicionada

--- Codigo/backend/backend/controller/professor_controller.py
+++ Codigo/backend/backend/controller/professor_controller.py
@@ -1,6 +1,7 @@
 import json
-from model import Professor
-from util import CAMINHO_USUARIOS
+from model import Professor, Disciplina
+
+from util import CAMINHO_USUARIOS, CAMINHO_DISCIPLINAS, ler_arquivo
 
 
 def cadastrar_professor(nome: str, email: str, senha: str, cpf: str):
@@ -13,3 +14,15 @@ def cadastrar_professor(nome: str, email: str, senha: str, cpf: str):
 
     with open(CAMINHO_USUARIOS, "w") as f:
         json.dump(data, f, indent=4)
+
+def listarAlunos(disciplina: Disciplina, disciplina_selecionada: str):
+    disciplinas: dict = ler_arquivo(caminho=CAMINHO_DISCIPLINAS)
+
+    for _, disciplina in disciplinas.items():
+        if disciplina["nome"] == disciplina_selecionada:
+            print(f"Disciplina: {disciplina["nome"]}, Ativa: {disciplina["ativa"]}")
+            print("Alunos:")
+            for aluno in disciplina["alunos"]:
+                print(f"Nome: {aluno['nome']}, Email: {aluno['email']}, CPF: {aluno['cpf']}")
+
+    print("Usuários listados com sucesso!")
--- Codigo/backend/backend/view/professor_view.py
+++ Codigo/backend/backend/view/professor_view.py
@@ -1,5 +1,41 @@
 from util import menu
 from view import login_view
+from model import Disciplina
+from controller import professor_controller
+from util import (
+    CAMINHO_CURSOS,
+    CAMINHO_DISCIPLINAS,
+    divider,
+    ler_arquivo,
+    menu,
+    pedir_dados_usuario,
+)
 
 def menu_professor():
-    menu(header="Bem vindo, Professor(a)!", opcoes={}, voltar=login_view.menu_login)
+    menu(
+        header="Bem vindo, Professor(a)!",
+        opcoes={
+            "Alunos cadastrados por disciplina": lista_alunos
+        },
+        voltar=login_view.menu_login,
+    )
+
+def lista_alunos():
+    divider()
+    print("Listar alunos em suas disciplinas")
+    disciplina_selecionada = input("Digite o nome da disciplina que você quer ver os alunos cadastrados: ")
+    disciplinas: dict = ler_arquivo(caminho=CAMINHO_DISCIPLINAS)
+    
+    if disciplina_selecionada not in disciplinas:
+        print("Disciplina não encontrada.")
+        lista_alunos()
+        return
+
+
+    disciplina: Disciplina = Disciplina.model_validate(disciplinas[disciplina_selecionada])
+    print()
+    professor_controller.listarAlunos(disciplina=disciplina, disciplina_selecionada=disciplina_selecionada)
+    
+    
+    
+    
